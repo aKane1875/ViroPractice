132d158c55420d4226ce3fa8e9e64543541a896f
Allan Kane <allanhfc@yahoo.co.uk>
2022-01-19 14:51:46 +0000

    initial commit

---
+++ js/HelloWorldSceneAR.js
@@ -0,0 +1,62 @@
+"use strict";
+
+import React, { Component } from "react";
+
+import { StyleSheet } from "react-native";
+
+import { ViroARScene, ViroText, ViroConstants, Viro3DObject } from "react-viro";
+
+export default class HelloWorldSceneAR extends Component {
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: "Initializing AR...",
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
+    return (
+      <ViroARScene onTrackingUpdated={this._onInitialized}>
+        <ViroText
+          text={this.state.text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
+        />
+        <Viro3DObject
+          source={{ uri: "http://example.org/myobject.obj" }}
+          position={[-0.0, -5.5, -1.15]}
+          // materials={["heart"]}
+          type="OBJ"
+        />
+      </ViroARScene>
+    );
+  }
+
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text: "MONSTER MONSTER",
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
+
+var styles = StyleSheet.create({
+  helloWorldTextStyle: {
+    fontFamily: "Arial",
+    fontSize: 30,
+    color: "#ffffff",
+    textAlignVertical: "center",
+    textAlign: "center",
+  },
+});
+
+module.exports = HelloWorldSceneAR;
